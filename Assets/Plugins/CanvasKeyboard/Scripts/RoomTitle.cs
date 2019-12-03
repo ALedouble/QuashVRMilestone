0725f6c0441d63a4c4c9b06ab859514781dc375c
Tristan <t.veille@rubika-edu.com>
2019-12-03 14:20:10 +0100

    HELP

---
+++ Assets/Plugins/CanvasKeyboard/Scripts/RoomTitle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RoomTitle : MonoBehaviour
+{
+    public TextMeshProUGUI roomNameCreated;
+    public TextMeshProUGUI roomNameDisplay;
+
+    void Update()
+    {
+        DisplayRoomName();
+    }
+
+    void DisplayRoomName ()
+    {
+        roomNameDisplay.text = roomNameCreated.ToString();
+    }
+}

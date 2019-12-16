a0ca52a9c0859b9f786ecb0b3ee250cdb985da68
SalleVR-2 <t.veille@rubika-edu.com>
2019-12-16 18:28:20 +0100

    WIP Ajout de l'Input Manager
    - Manque : implementation NPlayerManager (logique des inputs),  implementation des inputs en InputMod.MENU

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum InputMod
+{
+    MENU,
+    GAMEPLAY
+}
+
+public class InputManager : MonoBehaviour
+{
+    public InputMod inputMod = InputMod.GAMEPLAY;       //A cacher dans l'inspector
+
+    public void SetInputMod(InputMod inputMod)
+    {
+        this.inputMod = inputMod;
+    }
+
+    public InputMod GetInputMod()
+    {
+        return inputMod;
+    }
+
+    public void OnRightTriggerPress()
+    {
+        if(inputMod == InputMod.GAMEPLAY)
+        {
+            NPlayerManager.instance.RightHandActionCall();
+        }
+        else if(inputMod == InputMod.MENU)
+        {
+            //Ici Tristan
+        }
+    }
+
+    public void OnRightTriggerRelease()
+    {
+        if (inputMod == InputMod.GAMEPLAY)
+        {
+            NPlayerManager.instance.RightHandActionEnd();
+        }
+        else if (inputMod == InputMod.MENU)
+        {
+            //Ici Tristan
+        }
+    }
+
+    public void OnLeftTriggerPress()
+    {
+        if (inputMod == InputMod.GAMEPLAY)
+        {
+            NPlayerManager.instance.LeftHandActionCall();
+        }
+        else if (inputMod == InputMod.MENU)
+        {
+            //Ici Tristan
+        }
+    }
+
+    public void OnLeftTriggerRelease()
+    {
+        if (inputMod == InputMod.GAMEPLAY)
+        {
+            NPlayerManager.instance.LeftHandActionEnd();
+        }
+        else if (inputMod == InputMod.MENU)
+        {
+            //Ici Tristan
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/NPlayerManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPlayerManager : MonoBehaviour
+{
+    #region Singleton
+
+    public static NPlayerManager instance;
+
+    private void Awake()
+    {
+        if (instance)
+            Destroy(this);
+        else
+            instance = this;
+    }
+    #endregion
+
+    //Player settings (Gaucher/Droitier, pouvoirs utilises, ...)
+
+
+
+    public void RightHandActionCall()
+    {
+
+    }
+
+    public void LeftHandActionCall()
+    {
+
+    }
+
+    public void RightHandActionEnd()
+    {
+
+    }
+
+    public void LeftHandActionEnd()
+    {
+
+    }
+
+    //Ajouter les super?
+}

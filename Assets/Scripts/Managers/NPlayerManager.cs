a5dc1e89bb113647094d76b06990c6cf7bbaced9
Rubika-TPiau <t.piau@Rubika-edu.com>
2019-12-16 22:09:27 +0100

    NPlayerManager Implementer

--- Assets/Scripts/Managers/NPlayerManager.cs
+++ Assets/Scripts/Managers/NPlayerManager.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum MainHand
+{
+    RIGHTHANDED,
+    LEFTHANDED
+}
+
 public class NPlayerManager : MonoBehaviour
 {
     #region Singleton
@@ -18,28 +24,43 @@ public class NPlayerManager : MonoBehaviour
     #endregion
 
     //Player settings (Gaucher/Droitier, pouvoirs utilises, ...)
-
-
+    public MainHand mainHand = MainHand.RIGHTHANDED;
 
     public void RightHandActionCall()
     {
-
+        if(mainHand == MainHand.RIGHTHANDED)
+        {
+            RacketManager.instance.EnterEmpoweredState();
+            //Anime de malade!!!
+        }
     }
 
     public void LeftHandActionCall()
     {
-
+        if (mainHand == MainHand.LEFTHANDED)
+        {
+            RacketManager.instance.EnterEmpoweredState();
+            //Anime de malade!!!
+        }
     }
 
     public void RightHandActionEnd()
     {
-
+        if (mainHand == MainHand.RIGHTHANDED)
+        {
+            RacketManager.instance.ExitEmpoweredState();
+            //Anime bof mais bien quand meme, hein!!!
+        }
     }
 
     public void LeftHandActionEnd()
     {
-
+        if (mainHand == MainHand.LEFTHANDED)
+        {
+            RacketManager.instance.ExitEmpoweredState();
+            //Anime bof mais bien quand meme, hein!!!
+        }
     }
 
-    //Ajouter les super?
+    //Ajouter les supers
 }

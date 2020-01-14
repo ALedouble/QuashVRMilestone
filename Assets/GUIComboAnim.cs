942b531b5e84a99e26c4978aaf2e4b04fb23e5e7
SalleVR-2 <t.veille@rubika-edu.com>
2020-01-14 19:09:48 +0100

    FIX Test HUD / UI in VR

---
+++ Assets/GUIComboAnim.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIComboAnim : MonoBehaviour
+{
+    #region Singleton
+    public static GUIComboAnim instance;
+
+    public void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+
+    [Header("Animation")]
+    public Animator animCombo;
+
+    // PLAY ANIM FUNCTIONS // 
+    public void PlayAnimComboIncreasing()
+    {
+        animCombo.Play("A_Combo_Increasing");
+    }
+
+    public void PlayAnimComboBreak()
+    {
+        animCombo.Play("A_Combo_Break");
+    }
+}
--- Assets/Scripts/UI/GUIScoreData.cs
+++ Assets/Scripts/UI/GUIScoreData.cs
@@ -4,27 +4,8 @@ using UnityEngine;
 
 public class GUIScoreData : GUIComponent
 {
-    #region Singleton
-    public static GUIScoreData instance;
-
-    public void Awake()
-    {
-        instance = this;
-    }
-    #endregion
-
-    [Header("Animation")]
-    public Animator animScore;
-
     public override void UpdateText(string newText)
     {
         base.UpdateText(newText);
     }
-
-    // PLAY ANIM FUNCTIONS // 
-    public void PlayAnimScoreIncrease()
-    {
-        animScore.Play("A_Score_Increasing");
-    }
-
 }

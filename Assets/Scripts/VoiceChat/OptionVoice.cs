f9c2e8301b77e5a88abe4ce724356d69b1244b2e
AlexisLedouble <a.ledouble@rubika-edu.com>
2020-01-17 10:53:01 +0100

    Option voice chat

---
+++ Assets/Scripts/VoiceChat/OptionVoice.cs
@@ -0,0 +1,34 @@
+﻿using Dissonance;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionVoice : MonoBehaviour
+{
+    public GameObject dissonance;
+    DissonanceComms comms;
+    VoiceBroadcastTrigger vbt;
+
+    private void Awake()
+    {
+        comms = dissonance.GetComponent<DissonanceComms>();
+        vbt = dissonance.GetComponent<VoiceBroadcastTrigger>();
+
+        IsMuted();
+    }
+
+    private void Update()
+    {
+        Debug.Log(comms.IsMuted);
+    }
+
+    private void IsMuted()
+    {
+        comms.IsMuted = true;
+    }
+
+    private void SoundVolume()
+    {
+        comms.RemoteVoiceVolume = 0.5f;
+    }
+}

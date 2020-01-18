295a839b8badc04bbb5fb28f447594335e083f39
Tristan <t.veille@rubika-edu.com>
2020-01-18 15:36:12 +0100

    hud modifications wip

--- Assets/Scripts/UI/GUIHUD.cs
+++ Assets/Scripts/UI/GUIHUD.cs
@@ -10,8 +10,36 @@ public class GUIHUD : MonoBehaviour
     [SerializeField] GUIFillBarData fillBarData;
     [SerializeField] GUITimerData timerData;
 
+    [Header("HUD ScoreScreen References")]
+    [SerializeField] GUIMaxScoreData maxScoreData;
+    [SerializeField] GUIMaxComboData maxComboData;
+
+    [Header("References")]
+    [SerializeField] GameObject scoreScreen;
+    [SerializeField] GameObject scoreScreenCompleted;
+    [SerializeField] GameObject scoreScreenFailed;
+    [SerializeField] GameObject scoreComboHUD;
+    [SerializeField] GameObject timerHUD;
+    [SerializeField] GameObject layerCountHUD;
+
+    #region Singleton
+    public static GUIHUD instance;
+
+    public void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+
     public GUIScoreData[] ScoreData { get => scoreData; }
     public GUIComboData[] ComboData { get => comboData; }
     public GUIFillBarData FillBarData { get => fillBarData; }
     public GUITimerData TimerData { get => timerData; }
+    public GUIMaxScoreData MaxScoreData { get => maxScoreData; }
+    public GUIMaxComboData MaxComboData { get => maxComboData; }
+
+    // ----------------------------------------------------- //
+
+   
+
 }

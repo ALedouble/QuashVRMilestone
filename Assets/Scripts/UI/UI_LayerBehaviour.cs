b005d43d716a13bd7639397b9d4eb685920df8a8
Tristan <t.veille@rubika-edu.com>
2020-01-18 20:14:57 +0100

    HUD Layer Completed Animation

--- Assets/Scripts/UI/UI_LayerBehaviour.cs
+++ Assets/Scripts/UI/UI_LayerBehaviour.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class UI_LayerBehaviour : MonoBehaviour
 {
     public GameObject layerCompleteObj;
+    public Animator animLayerCompleted;
 
 
     public void CompleteLayer()
     {
-        layerCompleteObj.SetActive(true);
+        //layerCompleteObj.SetActive(true);
+        animLayerCompleted.Play("A_HUD_Layer_Completed");
     }
 }

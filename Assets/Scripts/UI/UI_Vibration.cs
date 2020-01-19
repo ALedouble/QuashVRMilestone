f110375a6d579522961499cc828811fdf8d4398f
SalleVR-2 <t.veille@rubika-edu.com>
2020-01-19 15:39:26 +0100

    Vibration UI problem
    J'en ai marre je passe à autre chose

---
+++ Assets/Scripts/UI/UI_Vibration.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class UI_Vibration : MonoBehaviour
+{
+    public VRTK_UIPointer uip;
+    private VRTK_ControllerReference controllerRef;
+
+    public void VibrateOn(float strength)
+    {
+        controllerRef = VRTK_ControllerReference.GetControllerReference(uip.controllerEvents.gameObject);
+
+        VRTK_ControllerHaptics.TriggerHapticPulse(controllerRef, strength);
+    }
+
+    public void PRINTING(string text)
+    {
+        Debug.Log(text);
+    }
+}

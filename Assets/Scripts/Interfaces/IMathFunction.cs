ee9268fc12f4646155db0c076ea060f508b75d27
PIAU Timothée <t.piau@rubika-edu.com>
2020-01-20 22:17:06 +0100

    Fix switch target reseau + Base Spawn/Despawn ball + Base soft pause

---
+++ Assets/Scripts/Interfaces/IMathFunction.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IMathFunction
+{
+    float Evaluate(float x);
+}
---
+++ Assets/Scripts/Utility/SequantMethod.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequantMethod : INumericalSolver
+{
+    private float xn;
+    private float xnMinus1;
+    private float xnMinus2;
+
+    public SequantMethod(float xn, float xnMinus1)
+    {
+        this.xn = xn;
+        this.xnMinus1 = xnMinus1;
+    }
+
+    public float Solve(IMathFunction f)
+    {
+        float currentFunctionEvaluation = f.Evaluate(xn);
+
+        while ( currentFunctionEvaluation > 0.01 || currentFunctionEvaluation < -0.01)
+        {
+            xnMinus2 = xnMinus1;
+            xnMinus1 = xn;
+            xn = xnMinus1 - currentFunctionEvaluation * (xnMinus1 - xnMinus2) / (currentFunctionEvaluation - f.Evaluate(xnMinus2));
+        }
+
+        return xn;
+    }
+}

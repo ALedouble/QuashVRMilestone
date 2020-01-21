c7616e57fad198217b79b0d16ade8bc3cd61adf6
SalleVR-2 <t.veille@rubika-edu.com>
2020-01-21 21:54:44 +0100

    Add Foreign racket reference in RacketManager

--- Assets/Scripts/Utility/ForeignRacketColliderDisabler.cs
+++ Assets/Scripts/Utility/ForeignRacketColliderDisabler.cs
@@ -8,8 +8,9 @@ public class ForeignRacketColliderDisabler : MonoBehaviour
     private void Start()
     {
        if(!PhotonNetwork.OfflineMode && !(gameObject.GetComponent<PhotonView>().IsMine))
-        {
+       {
             gameObject.GetComponent<BoxCollider>().enabled = false;
-        }
+            RacketManager.instance.SetForeignPlayerRacket(gameObject);
+       }
     }
 }

2080ce1a3475b1b125335bdee993b7454e62922a
Tristan <t.veille@rubika-edu.com>
2020-01-24 19:17:56 +0100

    Fix menu name + level index + roomListings

--- Assets/Plugins/CanvasKeyboard/Scripts/RoomTitle.cs
+++ Assets/Plugins/CanvasKeyboard/Scripts/RoomTitle.cs
@@ -1,23 +1,9 @@
-﻿using Photon.Pun;
-using Photon.Realtime;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class RoomTitle : MonoBehaviour
 {
-    public TextMeshProUGUI roomNameCreated;
-    public TextMeshProUGUI roomNameDisplay;
+
     
-
-
-    void Update()
-    {
-        DisplayRoomName();
-    }
-
-    void DisplayRoomName ()
-    {
-    }
 }

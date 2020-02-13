8ef62b52aaf52ca9fdd08cf2b80f7c57f6fb22bd
SalleVR-2 <t.veille@rubika-edu.com>
2020-02-13 19:55:25 +0100

    Saving Stuff
    Ajout d'un RacketFX pour play les FX de switchColor sur la racket (pas encore implemente)

---
+++ Assets/RacketFX.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacketFX : MonoBehaviour
+{
+    public SwitchColorFX[] switchFX = new SwitchColorFX[2];
+
+    public void PlaySwitchColorFX(int colorID)
+    {
+        for (int i = 0; i < switchFX[colorID].burstFX.Length; i++)
+        {
+            switchFX[colorID].burstFX[i].Play();
+        }
+
+        for (int i = 0; i < switchFX[colorID].loopFX.Length; i++)
+        {
+            switchFX[colorID].loopFX[i].Play();
+        }
+    }
+
+    public void StopSwitchColorFX(int colorID)
+    {
+        for (int i = 0; i < switchFX[colorID].burstFX.Length; i++)
+        {
+            switchFX[colorID].burstFX[i].Play();
+        }
+
+        for (int i = 0; i < switchFX[colorID].loopFX.Length; i++)
+        {
+            switchFX[colorID].loopFX[i].Play();
+        }
+    }
+
+    [System.Serializable]
+    public struct SwitchColorFX
+    {
+        public ParticleSystem[] burstFX;
+        public ParticleSystem[] loopFX;
+    }
+}

f99cdde52f0a3ea8292b947284f7d703d5602020
AlexisLedouble <a.ledouble@rubika-edu.com>
2020-02-24 17:51:51 +0100

    modify rpc

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -125,9 +125,9 @@ public class GameManager : MonoBehaviour
             if (gameMod == GameMod.GAMEPLAY)
             {
                 Debug.Log(MultiLevel.Instance.levelIndex);
-                photonView.RPC("SelectionLevelRPC", RpcTarget.All, MultiLevel.Instance.levelIndex);
+              //  photonView.RPC("SelectionLevelRPC", RpcTarget.All, MultiLevel.Instance.levelIndex);
 
-                 // SelectionLevel(MultiLevel.Instance.levelIndex);
+                 SelectionLevelRPC(MultiLevel.Instance.levelIndex);
             }
         }
     }
@@ -250,7 +250,6 @@ public class GameManager : MonoBehaviour
     }
 
 
-    [PunRPC]
     public void SelectionLevelRPC(int selection){
         //LevelManager.instance.ConfigDistribution(selection);
         LevelManager.instance.StartLevelInitialization(selection);

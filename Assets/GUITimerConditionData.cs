01475ca6ce0d70775ac92a65df7e45483fb5365c
Tristan <t.veille@rubika-edu.com>
2020-02-28 18:19:34 +0100

    HUD1Player Timer Condition UI

---
+++ Assets/GUITimerConditionData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUITimerConditionData : GUIComponent
+{
+       public override void UpdateText(string newText)
+    {
+        base.UpdateText(newText);
+    }
+
+
+}

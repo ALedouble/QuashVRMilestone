72dd2fa6f93d5cc843ed4f378d4e1ccdb396faa3
Vince <v.trinel@rubika-edu.com>
2020-02-28 11:43:54 +0100

    Warning Windows

---
+++ Assets/Scripts/UI/WarningWindow/GUIWarningWindows.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GUIWarningWindows : MonoBehaviour
+{
+    [Header("Text values settings")]
+    [SerializeField] string windowTitle = "!!! Warning !!!";
+    [SerializeField] string windowText = "Warning Text";
+    [SerializeField] string buttonText = "Ok";
+
+    [Header("References")]
+    [SerializeField] TextMeshProUGUI textMeshTitle;
+    [SerializeField] TextMeshProUGUI textMeshText;
+    [SerializeField] TextMeshProUGUI textMeshButton;
+
+    //delegate
+    public delegate void WarningWindowDelegate();
+    public event WarningWindowDelegate OnButtonPressed;
+
+    protected virtual void OnEnable()
+    {
+        SetWarningText();
+    }
+
+    public virtual void SetWarningText()
+    {
+        textMeshTitle.text = windowTitle;
+        textMeshText.text = windowText;
+        textMeshButton.text = buttonText;
+    }
+
+    public virtual void OverrideText(string p_title, string p_text, string p_button = "Ok")
+    {
+        windowTitle = p_title;
+        windowText = p_text;
+        buttonText = p_button;
+    }
+
+    public virtual void OnValidateButtonClicked()
+    {
+        OnButtonPressed();
+    }
+
+}

941e4ff101d0d8d0bd3184103ed2160d418d9cbd
SalleVR-2 <t.veille@rubika-edu.com>
2020-06-22 15:35:26 +0200

    Restart Button Score screen solo

---
+++ Assets/Scripts/UI/UI_GameplayRestartButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_GameplayRestartButton : MonoBehaviour
+{
+    public void RestartGameplayScene()
+    {
+        GUILevelFade.instance.FadeOut();
+        StartCoroutine(AnimFade());
+    }
+
+    IEnumerator AnimFade()
+    {
+        yield return new WaitForSeconds(1.5f);
+        GameManager.Instance.RestartScene();
+    }
+}

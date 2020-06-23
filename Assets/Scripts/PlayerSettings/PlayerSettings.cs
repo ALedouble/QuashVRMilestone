a303f0dbab8b8d2179a06a827a110b98aa6dec85
SalleVR-2 <t.veille@rubika-edu.com>
2020-06-23 16:42:46 +0200

    Add LeftHanded basic code architectecture

---
+++ Assets/Scripts/PlayerSettings/PlayerSettings.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSettings : MonoBehaviour
+{
+    public static PlayerSettings Instance;
+    public PlayerHand PlayerDominantHand { get; set; }
+    //PlayerSize
+
+    private void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        //Default Need proper settings
+        PlayerDominantHand = PlayerHand.RIGHT;
+    }
+
+    public void LoadPlayerSettings()
+    {
+        
+    }
+
+    public void SavePlayerSettings()
+    {
+
+    }
+}

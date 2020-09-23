9bcbeaaef32d0856e3e21a2e417653e567fc9571
SalleVR-2 <t.veille@rubika-edu.com>
2020-09-23 17:43:01 +0200

    FirstTime UI + Option UI Implementation

---
+++ Assets/Scripts/UI/GUIFirstTimeConfirmationButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIFirstTimeConfirmationButton : MonoBehaviour
+{
+    public void OnButtonPress()
+    {
+        GUIFirstTimeMenu.Instance.ConfirmChoice();
+    }
+}

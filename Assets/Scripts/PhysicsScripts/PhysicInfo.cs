0bbc723ee5f6e499898bc7c194ba5e044c94d025
SalleVR-2 <t.veille@rubika-edu.com>
2020-09-24 18:30:15 +0200

    Phisic info longer duration sampling

--- Assets/Scripts/PhysicsScripts/PhysicInfo.cs
+++ Assets/Scripts/PhysicsScripts/PhysicInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class PhysicInfo : MonoBehaviour
 {
@@ -17,6 +18,44 @@ public class PhysicInfo : MonoBehaviour
 
     private float dTMinus1;
 
+
+    public float samplingDuration = 0.1f;
+
+    private List<Vector3> positions;
+    private List<Vector3> velocities;
+
+    private bool isVelocityCalculated;
+    private Vector3 calculatedVelocity;
+    public Vector3 Velocity
+    {
+        get
+        {
+            if (!isVelocityCalculated)
+            {
+                if (velocities.Count < 2)
+                {
+                    calculatedVelocity = Vector3.zero;
+                }
+                else
+                {
+                    calculatedVelocity = Vector3.zero;
+
+                    foreach (Vector3 velocity in velocities)
+                    {
+                        calculatedVelocity += velocity;
+                    }
+
+                    calculatedVelocity /= velocities.Count;
+
+                    isVelocityCalculated = true;
+                }
+            }
+
+            return calculatedVelocity;
+        }
+    }
+
+    private bool isOld = false;
     
     #region Save properties
 
@@ -50,11 +89,16 @@ public class PhysicInfo : MonoBehaviour
         rotationTMinus1 = transform.rotation;
 
         dTMinus1 = 1;
+
+        positions = new List<Vector3>();
+        velocities = new List<Vector3>();
+
+        isVelocityCalculated = true;
     }
 
     void FixedUpdate()
     {
-        if(!BallManager.instance.IsBallPaused)
+        if (!BallManager.instance.IsBallPaused)
         {
             velocityTMinusHalf = CalculateVelocity(transform.position, positionTMinus1, Time.fixedDeltaTime);
             angularVelocityTMinusHalf = CalculateAngularVelocity(transform.rotation, rotationTMinus1, Time.fixedDeltaTime);
@@ -69,8 +113,82 @@ public class PhysicInfo : MonoBehaviour
             angularVelocityTMinus3half = angularVelocityTMinusHalf;
 
             dTMinus1 = Time.fixedDeltaTime;
+
+
+            isVelocityCalculated = false;
+
+            AddPosition(transform.position);
+        }
+        else
+            ResetValues();
+    }
+
+    private void AddPosition(Vector3 newPosition)
+    {
+        bool isBufferFull = false;
+
+        positions.Add(newPosition);
+        if (positions.Count * Time.fixedDeltaTime > samplingDuration)
+        {
+            positions.RemoveAt(0);
+            isBufferFull = true;
+        }
+
+        UpdateVelocities(isBufferFull);
+    }
+
+    private void UpdateVelocities(bool shouldRemoveFirstElement)
+    {
+        if(positions.Count > 1)
+        {
+            velocities.Add( CalculateVelocity(positions[positions.Count - 1], positions[positions.Count - 2], Time.fixedDeltaTime) );
+            if (shouldRemoveFirstElement)
+                velocities.RemoveAt(0);
         }
     }
+
+    private void ResetValues()
+    {
+        positions.Clear();
+        velocities.Clear();
+    }
+
+    #region Getter
+
+    private Vector3 GetPosition()
+    {
+        return transform.position;
+    }
+
+    public Quaternion GetRotation()             // Prendre en compte la rotation de base de la raquette dans la main du joueur
+    {
+        return transform.rotation;
+    }
+
+    public Vector3 GetVelocity()
+    {
+        if (isOld)
+            return velocityTMinusHalf;
+        else
+            return Velocity;
+    }
+
+    public Vector3 GetAngularVelocity()         // Prendre en compte la rotation de base de la raquette dans la main du joueur
+    {
+        return angularVelocityTMinusHalf;
+    }
+
+    public Vector3 GetAcceleration()
+    {
+        return accelerationTMinus1;
+    }
+
+    public Vector3 GetAngularAcceleration()
+    {
+        return angularAccelerationTMinus1;
+    }
+
+    #endregion
 
     #region Calculation Methods
 
@@ -87,36 +205,6 @@ public class PhysicInfo : MonoBehaviour
     private Vector3 CalculateAcceleration(Vector3 velocity2, Vector3 velocity1, float deltaTime2, float deltaTime1)
     {
         return (velocity2 - velocity1) / ((deltaTime2 + deltaTime1) / 2);
-    }
-
-    private Vector3 GetPosition()
-    {
-        return transform.position;
-    }
-
-    public Quaternion GetRotation()             // Prendre en compte la rotation de base de la raquette dans la main du joueur
-    {
-        return transform.rotation;
-    }
-
-    public Vector3 GetVelocity()
-    {
-        return velocityTMinusHalf;
-    }
-
-    public Vector3 GetAngularVelocity()         // Prendre en compte la rotation de base de la raquette dans la main du joueur
-    {
-        return angularVelocityTMinusHalf;
-    }
-
-    public Vector3 GetAcceleration()
-    {
-        return accelerationTMinus1;
-    }
-
-    public Vector3 GetAngularAcceleration()
-    {
-        return angularAccelerationTMinus1;
     }
 
     #endregion

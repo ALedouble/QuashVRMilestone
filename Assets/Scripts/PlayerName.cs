5b3ab6b9389a7a5f34e712f4ec2a383ad3674eba
TimP <t.piau@rubika-edu.com>
2020-10-02 20:58:58 +0200

    Add PlayerName

---
+++ Assets/Scripts/PlayerName.cs
@@ -0,0 +1,30 @@
+﻿using Steamworks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuashPlayerName
+{
+    public static string UserName 
+    {
+        get
+        {
+            switch(BuildPlatformManager.Instance.targetBuildPlatform)
+            {
+                case TargetBuildPlatform.Dev:
+                    return "GOD";
+                    break;
+                case TargetBuildPlatform.Steam:
+                    if (SteamManager.Initialized)
+                        return SteamFriends.GetPersonaName();
+                    break;
+                case TargetBuildPlatform.Viveport:
+                    if (Viveport.User.IsReady(null) == 0)
+                        return Viveport.User.GetUserName();
+                    break;
+            }
+
+            return "WTF Alexis";
+        }
+    }
+}
